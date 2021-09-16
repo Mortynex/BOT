0cf24f9d203b471a91a5f6e1bb90594fc8c25392
Mortynex <Mortynex@users.noreply.github.com>
2021-09-16 22:31:50 +0200

    added ban and kick commands

---
+++ src/SlashCommands/admin/ban.ts
@@ -0,0 +1,54 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { Client, GuildMember } from "discord.js";
+import { SlashCommand, SlashCommandInteraction } from "../../Interfaces";
+import { slashCommandArgument } from "../../Types";
+
+export const command: SlashCommand = {
+	data: new SlashCommandBuilder()
+		.setName("ban")
+		.setDescription("bans a user")
+		.setDefaultPermission(true)
+		.addUserOption((option) =>
+			option.setName("target").setDescription("user to ban").setRequired(true)
+		)
+		.addStringOption((option) =>
+			option.setName("reason").setDescription("reason for the ban").setRequired(false)
+		),
+	run(
+		client: Client,
+		interaction: SlashCommandInteraction,
+		args: slashCommandArgument[]
+	) {
+        const { member, options } = interaction;
+		const target = options.getMember("target");
+		const reason = options.getString("reason");
+
+		if (!(target instanceof GuildMember)) {
+			return interaction.followUp({
+				content: "Invalid target",
+			});
+		}
+
+		if (
+			target.roles.highest.position >= member.roles.highest.position &&
+			!member.permissions.has("ADMINISTRATOR")
+		) {
+			return interaction.followUp({
+				content: "Your hierarchical position doesnt allow you to ban this user",
+			});
+		}
+		if (!target.bannable) {
+			return interaction.followUp({
+				content: "I dont have permissions to ban this user",
+			});
+		}
+
+		interaction.followUp({
+			content: `Succesfully banned the user <@${target.id}>`,
+		});
+
+		/*target.ban({
+			reason: reason === null ? undefined : reason,
+		});*/
+	},
+};
---
+++ src/SlashCommands/admin/kick.ts
@@ -0,0 +1,52 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { Client, GuildMember } from "discord.js";
+import { SlashCommand, SlashCommandInteraction } from "../../Interfaces";
+import { slashCommandArgument } from "../../Types";
+
+export const command: SlashCommand = {
+	data: new SlashCommandBuilder()
+		.setName("kick")
+		.setDescription("kicks a user")
+		.setDefaultPermission(true)
+		.addUserOption((option) =>
+			option.setName("target").setDescription("user to kick").setRequired(true)
+		)
+		.addStringOption((option) =>
+			option.setName("reason").setDescription("reason for the kick").setRequired(false)
+		),
+	run(
+		client: Client,
+		interaction: SlashCommandInteraction,
+		args: slashCommandArgument[]
+	) {
+		const { member, options } = interaction;
+		const target = options.getMember("target");
+		const reason = options.getString("reason");
+
+		if (!(target instanceof GuildMember)) {
+			return interaction.followUp({
+				content: "Invalid target",
+			});
+		}
+
+		if (
+			target.roles.highest.position >= member.roles.highest.position &&
+			!member.permissions.has("ADMINISTRATOR")
+		) {
+			return interaction.followUp({
+				content: "Your hierarchical position doesnt allow you to kick this user",
+			});
+		}
+		if (!target.kickable) {
+			return interaction.followUp({
+				content: "I dont have permissions to kick this user",
+			});
+		}
+
+		interaction.followUp({
+			content: `Succesfully kicked the user <@${target.id}>`,
+		});
+
+		//target.kick(reason === null ? undefined : reason);
+	},
+};
