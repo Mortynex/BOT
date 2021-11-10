701c98e39f53adfda53ba8d7ca0798eaa192cd46
Mortynex <Mortynex@users.noreply.github.com>
2021-11-10 17:35:49 +0100

    feat: extended client class api

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,3 +1,20 @@
-import { Client } from "discord.js";
+import { Client, ClientOptions } from "discord.js";
+import { EventManager } from "managers";
+import { info } from "util/logger";
+import { t } from "util/translator";
 
-export class KittyClient extends Client {}
+export class KittyClient extends Client {
+	public events: EventManager;
+
+	constructor(options: ClientOptions) {
+		super(options);
+
+		this.events = new EventManager(this);
+	}
+
+	public async init() {
+		await this.events.loadAll();
+
+		await this.login(process.env.DISCORD_BOT_TOKEN);
+	}
+}
