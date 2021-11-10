7831e84a9ad665c714ea9165f48cbc9deb47dc92
Mortynex <Mortynex@users.noreply.github.com>
2021-11-10 17:34:50 +0100

    feat: added event manager class

---
+++ src/managers/EventManager.ts
@@ -0,0 +1,69 @@
+import globRead from "tiny-glob";
+import { mix } from "ts-mixer";
+import { Event } from "interfaces";
+import { KittyEvent } from "structures/KittyEvent";
+import { EVENTS_DIR } from "paths";
+import { BaseStoreManager } from "managers/BaseStoreManager";
+import { BaseClientManager } from "managers/BaseClientManager";
+import { info, warn } from "util/logger";
+import { t } from "util/translator";
+import { parse } from "path";
+
+export interface EventManager
+	extends BaseStoreManager<string, KittyEvent>,
+		BaseClientManager {}
+
+@mix(BaseStoreManager)
+export class EventManager extends BaseClientManager {
+	async loadAll() {
+		const events = await globRead(EVENTS_DIR, { absolute: true });
+
+		for (const eventPath of events) {
+			const event = (await import(eventPath)) as Event<any>;
+			const { name, run } = event;
+
+			if (!name || !run) {
+				warn(
+					t("managers.event.eventMissingProperties", {
+						eventName: parse(eventPath).base,
+					})
+				);
+
+				continue;
+			}
+
+			const kittyEvent = new KittyEvent({
+				name,
+				handler: run.bind(null, this.client),
+			});
+
+			this.add(kittyEvent);
+		}
+
+		info(
+			t("managers.event.loadedAll", {
+				eventsCount: this.store.size,
+			})
+		);
+	}
+
+	add(event: KittyEvent) {
+		this.store.set(event.name, event);
+
+		event.subscribe(this.client);
+
+		info(
+			t("managers.event.addedEvent", {
+				eventName: event.name,
+			})
+		);
+	}
+
+	remove(event: KittyEvent): boolean {
+		const removedFromStore = this.store.delete(event.name);
+
+		const unsubscribed = event.unsubcribe(this.client);
+
+		return unsubscribed && removedFromStore;
+	}
+}
