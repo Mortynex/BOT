cb9b6c7bdf243f7d82003ef2fa8b16d419acbaf6
Mortynex <Mortynex@users.noreply.github.com>
2021-11-10 17:36:29 +0100

    feat: start client on script startup

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-import { t } from "./translator";
+import { t } from "./util/translator";
+import { info, warn, error, debug } from "./util/logger";
+import { KittyClient } from "client";
+import { Intents } from "discord.js";
+import { config as dotenvConfig } from "dotenv";
 
-console.log(t("main.starting"));
+dotenvConfig();
+
+info(t("main.starting"));
+
+const client = new KittyClient({
+	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+
+client.init();
