e03bf038997fabd2b305e6ff4bfed9a461b74f18
Mortynex <Mortynex@users.noreply.github.com>
2021-11-10 17:31:35 +0100

    feat: populated locales

--- locales/en.ts
+++ locales/en.ts
@@ -1,5 +1,18 @@
 export const en = {
+	// CONSOLE
 	main: {
 		starting: "Initializating bot...",
 	},
+	client: {
+		isReady: "Client is ready and logged in as {clientTag}",
+	},
+	managers: {
+		event: {
+			eventMissingProperties:
+				"Event {eventName} has missing properties and couldn't be loaded",
+			loadedAll: `Succesfully loaded {eventsCount} events`,
+			addedEvent: `Succesfully Loaded "{eventName}" event`,
+		},
+	},
+	// DISCORD
 } as const;
