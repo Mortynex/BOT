2dd0c8a86b96893c1f138349b82d7acbfb25992f
Mortynex <Mortynex@users.noreply.github.com>
2021-11-11 19:12:44 +0100

    feat: database manager

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,19 +1,23 @@
 import { Client, ClientOptions } from "discord.js";
-import { EventManager } from "managers";
+import { DatabaseManager, EventManager } from "managers";
 import { LocalStorage } from "node-localstorage";
 import { LOCALSTORAGE_PATH } from "paths";
 
 export class KittyClient extends Client {
 	public events: EventManager;
 	public localStorage: LocalStorage = new LocalStorage(LOCALSTORAGE_PATH);
+	public database: DatabaseManager;
 
 	constructor(options: ClientOptions) {
 		super(options);
 
 		this.events = new EventManager(this);
+		this.database = new DatabaseManager(this);
 	}
 
 	public async init() {
+		await this.database.init();
+
 		await this.events.loadAll();
 
 		await this.login(process.env.DISCORD_BOT_TOKEN);
---
+++ src/managers/DatabaseManager.ts
@@ -0,0 +1,18 @@
+import { ECommand } from "entities";
+import { createConnection } from "typeorm";
+import { BaseClientManager } from "./BaseClientManager";
+import { ECommandsManager } from "./ECommandsManager";
+
+export class DatabaseManager extends BaseClientManager {
+	public commands: ECommandsManager;
+
+	async init() {
+		await createConnection({
+			type: "better-sqlite3",
+			database: "./db.sqlite",
+			entities: [ECommand],
+		});
+
+		this.commands = new ECommandsManager(ECommand);
+	}
+}
