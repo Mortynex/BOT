6a257cde37bff846792bbbfe78a3f789130c7d8a
Mortynex <Mortynex@users.noreply.github.com>
2021-11-11 19:11:58 +0100

    feat: api application command type alias

--- src/typings.ts
+++ src/typings.ts
@@ -1,6 +1,9 @@
 import { EventHandler, EventName } from "interfaces";
+import { RESTPostAPIApplicationCommandsJSONBody } from "discord-api-types";
 
 export type KittyEventOptions = {
 	handler: EventHandler<any>;
 	name: EventName;
 };
+
+export type RawCommand = RESTPostAPIApplicationCommandsJSONBody;
