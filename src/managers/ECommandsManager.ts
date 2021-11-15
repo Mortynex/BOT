a34f2f099025e9d48cd8c97709d328996ce4a209
Mortynex <Mortynex@users.noreply.github.com>
2021-11-15 17:32:02 +0100

    feat: entity commands manager

--- src/managers/ECommandsManager.ts
+++ src/managers/ECommandsManager.ts
@@ -1,4 +1,32 @@
 import { ECommand } from "entities";
+import { KittyCommand } from "structures";
+import { hashString } from "util/crypto";
 import { BaseRepositoryManager } from "./BaseRepositoryManager";
 
-export class ECommandsManager extends BaseRepositoryManager<ECommand> {}
+export class ECommandsManager extends BaseRepositoryManager<ECommand> {
+	async saveCommands(commands: KittyCommand<true>[]) {
+		const ecommands = commands.map(command => this.createEntity(command));
+
+		return this.repository.save(ecommands);
+	}
+
+	async saveCommand(command: KittyCommand<true>) {
+		return this.repository.save([this.createEntity(command)]);
+	}
+
+	async getCommandByName(name: string) {
+		return this.repository.findOne(name);
+	}
+
+	async getCommandById(id: string) {
+		return this.repository.findOne({ id });
+	}
+
+	createEntity(command: KittyCommand<true>): ECommand {
+		return this.repository.create({
+			hash: command.getHash(),
+			name: command.name,
+			id: command.id,
+		});
+	}
+}
