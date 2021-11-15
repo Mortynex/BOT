0747fa5afb9ee57cd6f0e8bf4b7f957c25f0f87a
Mortynex <Mortynex@users.noreply.github.com>
2021-11-15 18:32:59 +0100

    feat: use command manager in client

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,18 +1,22 @@
 import { Client, ClientOptions } from "discord.js";
 import { DatabaseManager, EventManager } from "managers";
+import { CommandManager } from "managers";
 import { LocalStorage } from "node-localstorage";
 import { LOCALSTORAGE_PATH } from "paths";
+import { debug } from "util/logger";
 
 export class KittyClient extends Client<true> {
 	public events: EventManager;
 	public localStorage: LocalStorage = new LocalStorage(LOCALSTORAGE_PATH);
 	public database: DatabaseManager;
+	public commands: CommandManager;
 
 	constructor(options: ClientOptions) {
 		super(options);
 
 		this.events = new EventManager(this);
 		this.database = new DatabaseManager(this);
+		this.commands = new CommandManager(this);
 	}
 
 	public async init() {
@@ -21,5 +25,7 @@ export class KittyClient extends Client<true> {
 		await this.events.loadAll();
 
 		await this.login(process.env.DISCORD_BOT_TOKEN);
+
+		await this.commands.load();
 	}
 }
