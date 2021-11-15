a830cb971ad8b7996fddc376308039ec13c7cfeb
Mortynex <Mortynex@users.noreply.github.com>
2021-11-15 15:40:34 +0100

    feat: command glob paths

--- src/paths.ts
+++ src/paths.ts
@@ -1,3 +1,6 @@
 export const EVENTS_DIR = "src/events/*.{ts,js}";
 
+export const COMMANDS_CATEGORIES_DIR = "src/commands/*";
+export const COMMANDS_DIR = "src/commands/*/*.{ts,js}";
+
 export const LOCALSTORAGE_PATH = "data/localStorage";
