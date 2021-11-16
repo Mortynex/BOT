baa384f7f1ee6497df2a65e99bfc326513252337
Mortynex <Mortynex@users.noreply.github.com>
2021-11-16 15:31:24 +0100

    feat: hashObject func

--- src/util/crypto/index.ts
+++ src/util/crypto/index.ts
@@ -3,3 +3,7 @@ import { createHash } from "node:crypto";
 export function hashString(input: string): string {
 	return createHash("md5").update(input).digest("hex");
 }
+
+export function hashObject(input: Object): string {
+	return createHash("md5").update(JSON.stringify(input)).digest("hex");
+}
