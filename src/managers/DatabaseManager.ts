4efc074940d41df8681066314905aa80ad241206
Mortynex <Mortynex@users.noreply.github.com>
2021-11-24 19:26:06 +0100

    feat: removed command database entities

--- src/managers/DatabaseManager.ts
+++ src/managers/DatabaseManager.ts
@@ -1,19 +1,13 @@
-import { ECommand } from "entities";
 import { createConnection } from "typeorm";
 import { BaseClientManager } from "./BaseClientManager";
-import { ECommandsManager } from "./ECommandsManager";
 
 export class DatabaseManager extends BaseClientManager {
-	public commands: ECommandsManager;
-
 	async init() {
 		await createConnection({
 			type: "better-sqlite3",
 			database: "./data/db.sqlite",
-			entities: [ECommand],
+			entities: [],
 			synchronize: process.env.NODE_ENV === "development",
 		});
-
-		this.commands = new ECommandsManager(ECommand);
 	}
 }
